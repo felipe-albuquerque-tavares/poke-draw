10f97ea2c7f03c18037abe9bb86cee39b55dff8e
felipe-albuquerque-tavares <felipetavares2004@hotmail.com>
2026-02-04 10:17:19 -0300

    chore: initial commit

---
+++ resources/js/components/AppLogo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import { home } from '@/routes';
+import { Sparkles } from 'lucide-vue-next';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Link :href="home()" class="flex items-center gap-2 self-center">
+        <div
+            class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary/10 text-sidebar-primary-foreground"
+        >
+            <Sparkles class="size-4 text-white dark:text-white" />
+        </div>
+        <div class="ml-1 grid flex-1 items-center text-left text-xl">
+            <span class="mb-0.5 truncate leading-tight font-semibold text-white"
+                >PokéDraw</span
+            >
+        </div>
+    </Link>
+</template>
---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+import { dashboard, login, register } from '@/routes';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Sparkles, Gift } from 'lucide-vue-next';
+import { Button } from '@/components/ui/button';
+import AppLogo from '@/components/AppLogo.vue';
+withDefaults(
+    defineProps<{
+        canRegister: boolean;
+    }>(),
+    {
+        canRegister: true,
+    },
+);
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div
+        class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]"
+    >
+        <div class="flex w-full justify-center border-b-1">
+            <header
+                class="mb-6 flex w-full max-w-[335px] justify-between text-sm not-has-[nav]:hidden lg:max-w-[1450px]"
+            >
+                <AppLogo />
+                <nav class="flex items-center justify-end gap-4">
+                    <Link
+                        v-if="$page.props.auth.user"
+                        :href="dashboard()"
+                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                    >
+                        Coleçäo
+                    </Link>
+                    <template v-else>
+                        <Link
+                            :href="login()"
+                            class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            v-if="canRegister"
+                            :href="register()"
+                            class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                        >
+                            Register
+                        </Link>
+                    </template>
+                </nav>
+            </header>
+        </div>
+        <div
+            class="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0"
+        >
+            <main
+                class="flex w-full max-w-[335px] flex-col-reverse items-center justify-center gap-6 overflow-hidden rounded-lg text-center lg:max-w-4xl lg:flex-col"
+            >
+                <div class="flex flex-col gap-3">
+                    <h1 class="text-4xl font-bold text-white">
+                        Pokémon Diário
+                    </h1>
+                    <h4 class="text-xl text-white">
+                        Resgate um Pokémon aleatório todos os dias
+                    </h4>
+                </div>
+
+                <div
+                    class="flex h-full w-[270px] flex-col justify-between gap-6"
+                >
+                    <div
+                        data-slot="menu-skeleton"
+                        class="relative h-[330px] w-full"
+                    >
+                        <div
+                            class="absolute flex h-full w-full flex-col items-center justify-center gap-2 text-white"
+                        >
+                            <Gift
+                                class="h-20 w-20 animate-bounce ease-in-out"
+                            />
+                            O seu pokemon te espera...
+                        </div>
+                        <Skeleton
+                            class="h-full w-full flex-1"
+                            data-sidebar="menu-skeleton-text"
+                        />
+                    </div>
+                    <Button
+                        class="cursor-pointer"
+                        :disabled="!$page.props.auth.user"
+                        >Resgatar</Button
+                    >
+                </div>
+            </main>
+        </div>
+        <div class="hidden h-14.5 lg:block"></div>
+    </div>
+</template>
